92c2512067bb68f5ceaa723bd4140fab8688a2ca
DoanhNV <ngovandoanhbg@gmail.com>
2019-11-11 23:13:55 +0700

    20191111: [Networking][URL] parse, read and wirte with a URL

--- networking/src/main/java/doanhnv/url/URL_Class.java
+++ networking/src/main/java/doanhnv/url/URL_Class.java
@@ -1,9 +1,9 @@
 package doanhnv.url;
 
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.*;
 
 public class URL_Class {
 
@@ -31,6 +31,76 @@ public class URL_Class {
 //            url3: http://google.com:80/search
 //            url4: http://google.com/search
 //            URI create URL: http://google.com/search%20pqage#name=doanh
+        }
+    }
+
+    static class Test_ParseURL {
+        public static void main(String[] args) throws IOException {
+            URL url = new URL("http://oracle.com/search/title?name=doanh ngo van#loading");
+
+            System.out.println("Protocol: " + url.getProtocol());
+            System.out.println("getHost: " + url.getHost());
+            System.out.println("getPort: " + url.getPort());
+            System.out.println("getDefaultPort: " + url.getDefaultPort());
+            System.out.println("getFile: " + url.getFile());
+            System.out.println("getPath: " + url.getPath());
+            System.out.println("getAuthority: " + url.getAuthority());
+            System.out.println("getQuery: " + url.getQuery());
+            System.out.println("getRef: " + url.getRef());
+            System.out.println("getUserInfo: " + url.getUserInfo());
+            System.out.println("getContent: " + url.getContent());
+        }
+    }
+
+    static class Test_ReadingDirectlyFromURL {
+        public static void main(String[] args) throws IOException {
+            URL url = new URL("http://google.com");
+            InputStream inputStream = url.openStream();
+            InputStreamReader streamReader = new InputStreamReader(inputStream);
+
+            String content = "";
+            int character = -1;
+            while (( character = streamReader.read()) != -1) {
+                content += (char) character;
+            }
+            System.out.println(content);
+        }
+    }
+
+    static class Test_ConnectToAURL {
+        public static void main(String[] args) throws IOException {
+            URL googleURL = new URL("http://google.com");
+            URLConnection urlConnection = googleURL.openConnection();
+            urlConnection.connect();
+            System.out.println("Response header: " + urlConnection.getHeaderFields());
+
+            InputStream inputStream = urlConnection.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            String content = "";
+            int tmpCharacter = -1;
+            while ((tmpCharacter = inputStreamReader.read()) != -1) {
+                content += (char) tmpCharacter;
+            }
+            inputStreamReader.close();
+            System.out.println(content);
+
+            // Convert to HttpURLConnection
+            System.out.println("=============================================================================");
+            URL googleURL2 = new URL("http://google.com");
+            HttpURLConnection httpURLConnection = (HttpURLConnection) googleURL2.openConnection();
+            System.out.println("Request properties: " + httpURLConnection.getRequestProperties());
+            System.out.println("Response message: " + httpURLConnection.getResponseMessage());
+            System.out.println("Response code: " + httpURLConnection.getResponseCode());
+
+            InputStream httpInputStream = httpURLConnection.getInputStream();
+            InputStreamReader httpInputStreamReader = new InputStreamReader(httpInputStream);
+            String content1 = "";
+            int tmpCharacter1 = -1;
+            while ((tmpCharacter1 = httpInputStreamReader.read()) != -1) {
+                content1 += (char) tmpCharacter1;
+            }
+            httpInputStreamReader.close();
+            System.out.println(content1);
         }
     }
 }
